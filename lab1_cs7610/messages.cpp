fa62a6681258517ef1e3dfd3aecdcb472d43f74d
Pushyamik <kaveti.p@husky.neu.edu>
2018-10-07 15:56:35 -0400

    code for priority queue of messages, senidng datamessage

---
+++ lab1_cs7610/messages.cpp
@@ -0,0 +1,29 @@
+//
+// Created by pushyamik on 10/7/18.
+//
+#include <iostream>
+#include <queue>
+
+#include "messages.h"
+using namespace std;
+
+int main()
+{
+    priority_queue<DataMessage, vector<DataMessage>, CompareMessage> Q;
+
+
+    for (int i = 0; i < 5; ++i) {
+        DataMessage m{1,1,(uint32_t)i,1};
+        Q.push(m);
+
+        // insert an object in priority_queue by using
+        // the Person strucure constructor
+    }
+
+    while (!Q.empty()) {
+        DataMessage p = Q.top();
+        Q.pop();
+        cout << p.msg_id << " " << p.type << "\n";
+    }
+    return 0;
+}
