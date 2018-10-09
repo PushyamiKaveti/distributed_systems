2638a430adb7d827b7867dfd3dba1684f36eb134
Pushyamik <kaveti.p@husky.neu.edu>
2018-10-09 17:30:11 -0400

    debugging: fixed mesg-pq addition eror to the queue

--- lab1_cs7610/prj1_tm.cpp
+++ lab1_cs7610/prj1_tm.cpp
@@ -154,7 +154,7 @@ std::priority_queue<Mesg_pq, std::vector<Mesg_pq>, CompareMessage> reorder_queue
     return tmp_q;
 }
 // function to handle the received messages
-void handle_messages(uint32_t ty ,uint32_t pid, map<uint32_t , int> pid_sock_map, queue<uint32_t > mid_q, int fdmax, fd_set writefds, int receive_fd, int& a_seq, int& p_seq,priority_queue <Mesg_pq, vector<Mesg_pq>, CompareMessage> final_mesg_q, char* buf){
+void handle_messages(uint32_t ty ,uint32_t pid, map<uint32_t , int> pid_sock_map, queue<uint32_t > mid_q, int fdmax, fd_set writefds, int receive_fd, int& a_seq, int& p_seq, priority_queue <Mesg_pq, vector<Mesg_pq>, CompareMessage>& final_mesg_q, char* buf){
 
     printf(" with type : \"%d  \"\n", ty);
     switch(ty){
@@ -449,7 +449,7 @@ int main(int argc, char *argv[])
                         uint32_t b1;
                         memcpy(&b1 , &buf, sizeof(uint32_t));
                         //handle the message
-                        handle_messages(b1 ,pid,pid_sock_map, mid_q, fdmax, writefds, receive_fd ,agreed_seq,proposed_seq, final_mesg_q, buf);
+                        handle_messages(b1 ,pid,pid_sock_map, mid_q, fdmax, writefds, receive_fd ,agreed_seq,proposed_seq, final_mesg_q , buf);
 
 
                     }
