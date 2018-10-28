feed57b7978ad812ae990f378df62ac7e655cfcd
Pushyamik <kaveti.p@husky.neu.edu>
2018-10-28 19:13:21 -0400

    debug statement in get address

--- lab2_cs7610/membership_protocol.cpp
+++ lab2_cs7610/membership_protocol.cpp
@@ -116,7 +116,7 @@ void *get_in_addr(struct sockaddr *sa)
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
-
+    cout<<sa->sa_family<<"\n";
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
